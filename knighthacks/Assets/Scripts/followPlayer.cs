9e954f9f6b9cf4fc46b72da746296a3cc8f3528a
William Douglass <w.douglass1127@gmail.com>
2023-10-07 19:51:56 -0400

    Add hat

---
+++ knighthacks/Assets/Scripts/followPlayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null)
+    {
+        gameObject.transform.position = target.position;
+    }
+    }
+}

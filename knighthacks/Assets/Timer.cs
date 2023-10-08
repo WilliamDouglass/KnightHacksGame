a8d44bab3c57fd1c6b6134212d068b9897ef8806
Donny Akers <mobslayer205@gmail.com>
2023-10-08 02:24:31 -0400

    Add final touches

--- knighthacks/Assets/Timer.cs
+++ knighthacks/Assets/Timer.cs
@@ -7,8 +7,14 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    float timer = 200;
+    float timer = 0;
     private TextMeshProUGUI text;
+    bool isActive = true;
+
+    public void StopTimer()
+    {
+        isActive = false;
+    }
 
     private void Start()
     {
@@ -18,12 +24,13 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        //Debug.Log(timer);
-        int mins = Mathf.FloorToInt(timer / 60);
-        int seconds = Mathf.FloorToInt(timer - (mins * 60));
-        int millis = Mathf.FloorToInt(100 * (timer - (mins * 60) - seconds));
-        text.text = $"{mins:D2}:{seconds:D2}:{millis:D2}";
-
+        if (isActive)
+        {
+            timer += Time.deltaTime;
+            int mins = Mathf.FloorToInt(timer / 60);
+            int seconds = Mathf.FloorToInt(timer - (mins * 60));
+            int millis = Mathf.FloorToInt(100 * (timer - (mins * 60) - seconds));
+            text.text = $"{mins:D2}:{seconds:D2}:{millis:D2}";
+        }
     }
 }
